aa1a96bf1d77b5e9ea047ef1e70b6eff98e013a1
Colin Fendrick <colin.fendrick@gmail.com>
2018-03-22 07:50:19 -0400

    Modified other stuff

--- bootstrap-page05/tween.js
+++ bootstrap-page05/tween.js
@@ -1,22 +1,18 @@
-const tl_title = new TimelineLite();
-
-tl_title.to("html, body", 1, { scrollTop: 0 })
-.to("#main-title, .heading", 1.5, { opacity: 1 })
-.to("#subtitle", 1, { opacity: 1 })
-.to("#begin-btn", 1, { opacity: 1 })
-
-window.onload = () => {
-  tl_title.play();
-  $('.heading').css({ "display": "flex" })
-}
-
-const tl_personalForms = new TimelineLite();
-
-tl_personalForms.to("#personalForms-title", 1, { opacity: 1})
-.to(".personalForms", 1.5, {opacity: 1})
-
-// Clicking one answer will scroll to the next question
 $(document).ready(function() {
+  const timelines = {
+    title: new TimelineLite(),
+    pq: new TimelineLite(),
+    mq: new TimelineLite
+  }
+
+  //Stuff on initial loading
+  $('.intro').css({ "display": "flex" });
+  timelines.title.to("html, body", 0.75, { scrollTop: 200 })
+    .to("#main-title, .intro", 1.5, { opacity: 1 })
+    .to("#subtitle", 1.25, { opacity: 1 })
+    .to("#begin-btn", 1, { opacity: 1 });
+
+  // Handling the clicks and moving to next item
   $('a[href*="#"]')
   .not('[href="#"]')
   .not('[href="#0"]')
@@ -31,23 +27,32 @@ $(document).ready(function() {
       if (target.length) {
         event.preventDefault();
         $(target).css({ "display": "flex" })
-
-        TweenLite.to($(target), 1.5, { opacity: 1 })
+        
+        // Animating the fade-ins for each element
+        const id = target[0].id
+        if (id === 'pq' || id === 'mq') {
+          timelines[id].to(`#${id}, #${id}-title`, 1.5, { opacity: 1 })
+          .to(`#${id}-forms`, 1.5, { opacity: 1 })
+        } else {
+          TweenLite.to((target), 2, { opacity: 1 });
+        }
+        console.log(target, $(target))
 
         // Scrolls to the element & focuses it
         TweenLite.to($('html, body'), 1.5, {
-          scrollTop: target.offset().top,
+          scrollTop: target.offset().top - 200,
           onComplete: function() {
+            console
             $(target).focus();
-            if ($(target).is(":focus")) {
+            if (target.is(":focus")) {
               return false;
             } else {
-              $(target).attr('tabindex', '-1');
-              $(target).focus();
+              target.attr('tabindex', '-1');
+              target.focus();
             }
           }
         })
       }
     }
-  })
-})
+  });
+});
